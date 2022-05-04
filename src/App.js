c57f13684550212df5bba8a619416bf1a7bd4c71
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-05-04 21:59:14 +1200

    set up memo component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 // Components
 import StateHook from './Hooks/StateHook'
 import EffectHook from './Hooks/EffectHook'
+import MemoHook from './Hooks/MemoHook'
 
 function App() {
   return (
     <div className="App">
       {/* <StateHook /> */}
-      <EffectHook />
+      {/* <EffectHook /> */}
+      <MemoHook />
     </div>
   );
 }
---
+++ src/Hooks/MemoHook.js
@@ -0,0 +1,13 @@
+import React, { useMemo } from 'react'
+
+const MemoHook = () => {
+
+
+   return (
+      <div>
+         Memo working
+      </div>
+   )
+}
+
+export default MemoHook
