bbd6746fca890e196d01e5d64406dd2ee80ca307
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-05-04 22:06:20 +1200

    set up example code

--- src/Hooks/MemoHook.js
+++ src/Hooks/MemoHook.js
@@ -1,13 +1,39 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 
 const MemoHook = () => {
+  const [number, setNumber] = useState(0)
+  const [dark, setDark] = useState(false)
+  const themeStyle = {
+    backgroundColor: dark ? 'black' : 'white',
+    color: dark ? 'white' : 'black'
+  }
 
+  const slowFunction = (num) => {
+    console.log('calling slow function')
+    for (let i = 0; i <= 10000000000; i++) {
+      return num * 2
+    }
+  }
 
-   return (
-      <div>
-         Memo working
+  const douleNumber = slowFunction(number)
+
+  return (
+    <>
+      <input
+        type='number'
+        value={number}
+        onChange={e => setNumber(parseInt(e.target.value))}
+      />
+      <button
+        onClick={() => setDark(prevDark => !prevDark)}
+      >Change Theme</button>
+      <div
+        style={themeStyle}
+      >
+        {douleNumber}
       </div>
-   )
+    </>
+  )
 }
 
 export default MemoHook
